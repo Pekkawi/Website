1c29d7aaec0447553d5cfad3097b2249db70b860
Pekawi <eduard.dez@gmail.com>
2024-03-11 11:09:24 +0100

    Testing

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,13 +9,14 @@ const Home = () => {
         <h1> And Several Bootiful pictures of our boss Andrei</h1>
 
         <div className=" flex flex-1 max-sm:flex-col">
-          <Image
+          {/*
+            <Image
             src="/assets/images/Andrei1.jpg"
             width={300}
             height={300}
             alt="Andrei1"
             className="rounded-lg shadow-light-300 max-sm:my-20 sm:mx-10"
-          />
+          /> */}
           <Image
             src="/assets/images/Andrei2.png"
             width={300}
