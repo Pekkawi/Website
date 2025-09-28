606947ad9fc497608c96fa668e8c57161b5558b5
EduardLean <edlie22@student.sdu.dk>
2025-09-28 10:05:51 +0200

    [ADDED] trustHost to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
+export default NextAuth({
+  ...authConfig,
+  trustHost: true,
+}).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
