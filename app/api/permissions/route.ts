2ea8c8857a748b99337dcbe3a72f13e1698d33b0
EduardLean <edlie22@student.sdu.dk>
2025-09-28 13:57:26 +0200

    [REMOVED] Relative path

--- app/api/permissions/route.ts
+++ app/api/permissions/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: NextRequest) {
     const permissionsWithImageURL = permissions.map((perm) => {
       return {
         ...perm.toObject(),
-        image: `${process.env.NEXTAUTH_URL}/api/images/${perm.image}`, // Image route for fetching
+        image: `api/images/${perm.image}`, // Image route for fetching
       };
     });
 
