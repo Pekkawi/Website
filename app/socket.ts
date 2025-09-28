039c4e63b6e7d505f0ae034011edd1ab45bbc979
EduardLean <edlie22@student.sdu.dk>
2025-09-28 15:28:09 +0200

    [BUG] Websocket server cors policy

--- app/socket.ts
+++ app/socket.ts
@@ -3,22 +3,8 @@
 import { io } from 'socket.io-client';
 
 // CRITICAL: Don't hardcode IPs!
-const getSocketURL = () => {
-  if (typeof window === 'undefined') {
-    return undefined;
-  }
 
-  // In production/Docker: use current origin
-  if (process.env.NODE_ENV === 'production') {
-    return window.location.origin;
-  }
-
-  // In development: use localhost
-  return 'http://localhost:3000';
-};
-
-export const socket = io(getSocketURL(), {
-  path: '/socket.io/',
+export const socket = io('', {
   withCredentials: true,
   transports: ['websocket', 'polling'],
 });
