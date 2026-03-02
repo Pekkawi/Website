2475112b2d9a985b8f9cd684b9c4ff6c3ad37f2a
EduardLean <edlie22@student.sdu.dk>
2026-03-02 10:18:24 +0100

    Adjusted user model to take into account when the user might not have a card_id/card_number

--- database/user.model.ts
+++ database/user.model.ts
@@ -1,65 +1,67 @@
-import { Schema, models, model} from "mongoose";
-import { IUser } from "@/interfaces/database.interfaces";
+import { Schema, models, model } from 'mongoose';
+import { IUser } from '@/interfaces/database.interfaces';
 
-const roles = ["user", "maintainer", "admin"];
+const roles = ['user', 'maintainer', 'admin'];
 const [defaultRole] = roles;
 
-
-const userSchema = new Schema({
-  azure_id: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  card_id: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  card_number: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  first_name: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  last_name: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  display_name: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  role: {
-    type: String,
-    required: true,
-    unique: false,
-    enum: roles,
-    default: defaultRole,
-  },
-  permissions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "permissions",
-      required: false,
-      unique: false,
-      default: [],
+const userSchema = new Schema(
+  {
+    azure_id: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    card_id: {
+      type: String,
+      unique: true,
+    },
+    card_number: {
+      type: String,
+      unique: false,
+    },
+    first_name: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    display_name: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    role: {
+      type: String,
+      required: true,
+      unique: false,
+      enum: roles,
+      default: defaultRole,
+    },
+    permissions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'permissions',
+        required: false,
+        unique: false,
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: { createdAt: 'created', updatedAt: 'updated' },
+  }
+);
 
-const User = models.users || model<IUser>("users", userSchema, "users");
+const User = models.users || model<IUser>('users', userSchema, 'users');
 
 // Check if the model already exists , otherwise create a model based on the schema
 
